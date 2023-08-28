5307900511748c6720f30121de21f4272d3f18a9
yvonne11yuting <yvonne11yuting@gmail.com>
2023-08-28 10:04:06 +0800

    add service and repository

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,23 @@
+import { MessagesRepository } from "./messages.repository";
+
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+
+  constructor() {
+    // Service is creating its own dependencies
+    // DONT DO THIS ON REAL APPS
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  async findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  async create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
